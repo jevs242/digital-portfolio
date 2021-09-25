b09d16d5754412337a1ce97a0d78b5144e490476
Jevspr <64289872+Jevspr@users.noreply.github.com>
2021-09-24 23:47:10 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Personal - v2.5.1
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
@@ -115,24 +110,6 @@
     });
   }, {
     offset: '80%'
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
   });
 
   // Porfolio isotope and filter
